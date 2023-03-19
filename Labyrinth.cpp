3569b22b8c9690e6ce3f2e023802e9b951c69fd6
Dm4rt <danmartinez2004@gmail.com>
2023-03-19 10:49:24 -0400

    finished milestone 1

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -1,8 +1,54 @@
 #include "Labyrinth.h"
+#include <iostream>
 
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
-    /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
+    bool spell=false;
+    bool pot=false;
+    bool wand=false;
+    for(int x=0;x<=moves.length();x++){
+    	
+    	//Check for item
+    	if(start->whatsHere==Item::SPELLBOOK){
+    		spell=true;
+    	}
+    	else if(start->whatsHere==Item::POTION){
+    		pot=true;
+    	}
+    	else if(start->whatsHere==Item::WAND){
+    		wand=true;
+    	}
+    	
+    	if(x==moves.length()){
+    		//Just we can check for items on the last move as well
+    		break;
+    	}
+    	char dir=moves[x];
+    	
+    	//Make move
+    	if(dir=='N'){
+    		if(start->north!=nullptr){
+    			start=start->north;
+    		}
+    	}
+    	else if(dir=='E'){
+    		if(start->east!=nullptr){
+    			start=start->east;
+    		}
+    	}
+    	else if(dir=='S'){
+    		if(start->south!=nullptr){
+    			start=start->south;
+    		}
+    	}
+    	else if(dir=='W'){
+    		if(start->west!=nullptr){
+    			start=start->west;
+    		}
+    	}
+    }
+    //std::cout<<spell<<" "<<pot<<" "<<wand<<std::endl;
+    if(spell&&pot&&wand){
+    	return true;
+    }
     return false;
 }
